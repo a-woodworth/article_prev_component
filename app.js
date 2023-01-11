b04d8af51d2065104a3a8192156c8fc1ae09b0e7
Amy Woodworth <woodworthcommunications@gmail.com>
2023-01-11 13:04:25 -0600

    Refactor app.js to fix aria-pressed issue and improve trapping focus in model.

--- app.js
+++ app.js
@@ -7,8 +7,13 @@ const lastFocusableItem = focusableItems[focusableItems.length - 1];
 
 function toggleModal() {
   // Update button attribute and styles
-  shareButton.setAttribute('aria-pressed', true);
   shareButton.classList.toggle('btn__modal-open');
+
+  if (shareButton.classList.contains('btn__modal-open')) {
+    shareButton.setAttribute('aria-pressed', true);
+  } else {
+    shareButton.setAttribute('aria-pressed', false);
+  }
 
   // Show modal content
   socialMediaModal.classList.toggle('hidden');
@@ -26,11 +31,23 @@ socialMediaModal.addEventListener('keydown', (e) => {
   }
 });
 
-// If modal active and first link has focus, listen for shift 
-// key to move focus to button and not card links
+// Trap focus in modal
 socialMediaModal.addEventListener('keydown', (e) => {
-  if ( e.key === 'Shift' && document.activeElement === firstFocusableItem) {
-    lastFocusableItem.focus();
+  if (e.key === 'Tab' || e.code === 'Tab') {
+    // if shift + tab on first link, focus goes to button
+    if ( e.shiftKey ) {
+      if ( document.activeElement === firstFocusableItem ) {
+        lastFocusableItem.focus();
+        e.preventDefault();
+      }
+      // if tab on last link, focus back to first link
+      else {
+        if (document.activeElement === lastFocusableItem ) {
+          firstFocusableItem.focus();
+          e.preventDefault();
+        }
+      }
+    }
   }
 });
 
